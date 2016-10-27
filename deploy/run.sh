ef1e094c36d92e4f5a7e94e11b675e69bc0fb3e5
Greg Walker <mgwalker@gmail.com>
2016-10-26 20:00:02 -0500

    I don't know how else to debug this...

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+echo "de beginning"
+cat deploy/deploy.key
+echo "Whoop whoop"
 eval "$(ssh-agent -s)" # Start the ssh agent
+echo "two"
 chmod 600 deploy/deploy.key # This key should have push access
+echo "three"
 ssh-add deploy/deploy.key
+echo "four"
 git remote add deploy dokku@darkcooger.net:jotc-staging.darkcooger.net
+echo "five"
 git push deploy master
+echo "six"
