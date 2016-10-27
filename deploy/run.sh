87c486a140664e92e3cda840da9f6de73945adf3
Greg Walker <mgwalker@gmail.com>
2016-10-26 19:56:13 -0500

    Maybe this?

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)" # Start the ssh agent
 chmod 600 deploy/deploy.key # This key should have push access
 ssh-add deploy/deploy.key
 git remote add deploy dokku@darkcooger.net:jotc-staging.darkcooger.net
-git push deploy development:master
+git push deploy master
